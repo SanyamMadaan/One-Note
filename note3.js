a43ba6a6add81b5366553871f49f23c0f6274779
SanyamMadaan <124160436+SanyamMadaan@users.noreply.github.com>
2023-02-01 19:00:09 +0530

    Add files via upload

---
+++ note3.js
@@ -0,0 +1,31 @@
+alert("lets check");
+const addBtn=document.querySelector('#addBtn');
+const main=document.querySelector('#main');
+const savenotes=()=>{
+
+
+
+
+
+    
+}
+addBtn.addEventListener("click",function(){
+    addNote();
+})
+const addNote=()=>{
+const note=document.createElement("div");
+note.classList.add('note');
+note.innerHTML=` <div class="tool"> 
+<i class=" trash fa-solid fa-trash"></i>
+<i class="save fa-solid fa-floppy-disk"></i>
+</div>
+<textarea > </textarea>
+`;
+main.appendChild(note);
+note.querySelector('.trash').addEventListener('click',function(){
+    note.remove();
+ 
+}
+)
+note.querySelector('.save').addEventListener('click',function(){savenotes()})
+} 
